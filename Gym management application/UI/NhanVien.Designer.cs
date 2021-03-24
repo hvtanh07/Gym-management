61fac4f1e5bc2c5d959e369578a500031c627617
duyducpm0120 <63636669+ducduypm0120@users.noreply.github.com>
2021-03-24 16:44:00 +0700

    Temporary push

---
+++ Gym management application/UI/NhanVien.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Gym_management_appication.UI
+{
+    partial class NhanVien
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxTen = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxSDT = new System.Windows.Forms.TextBox();
+            this.textBoxDiaChi = new System.Windows.Forms.TextBox();
+            this.textBoxChucVu = new System.Windows.Forms.TextBox();
+            this.textBoxLuong = new System.Windows.Forms.TextBox();
+            this.checkBoxNam = new System.Windows.Forms.CheckBox();
+            this.checkBoxNu = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên nhân viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Giới tính";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(523, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Số điện thoại";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(526, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Địa chỉ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(523, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Chức vụ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(526, 137);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Lương";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(112, 17);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(330, 22);
+            this.textBoxID.TabIndex = 8;
+            // 
+            // textBoxTen
+            // 
+            this.textBoxTen.Location = new System.Drawing.Point(112, 53);
+            this.textBoxTen.Name = "textBoxTen";
+            this.textBoxTen.Size = new System.Drawing.Size(330, 22);
+            this.textBoxTen.TabIndex = 9;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(112, 137);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(330, 22);
+            this.textBoxEmail.TabIndex = 11;
+            // 
+            // textBoxSDT
+            // 
+            this.textBoxSDT.Location = new System.Drawing.Point(620, 17);
+            this.textBoxSDT.Name = "textBoxSDT";
+            this.textBoxSDT.Size = new System.Drawing.Size(330, 22);
+            this.textBoxSDT.TabIndex = 12;
+            // 
+            // textBoxDiaChi
+            // 
+            this.textBoxDiaChi.Location = new System.Drawing.Point(620, 53);
+            this.textBoxDiaChi.Name = "textBoxDiaChi";
+            this.textBoxDiaChi.Size = new System.Drawing.Size(330, 22);
+            this.textBoxDiaChi.TabIndex = 13;
+            // 
+            // textBoxChucVu
+            // 
+            this.textBoxChucVu.Location = new System.Drawing.Point(620, 98);
+            this.textBoxChucVu.Name = "textBoxChucVu";
+            this.textBoxChucVu.Size = new System.Drawing.Size(330, 22);
+            this.textBoxChucVu.TabIndex = 14;
+            // 
+            // textBoxLuong
+            // 
+            this.textBoxLuong.Location = new System.Drawing.Point(620, 137);
+            this.textBoxLuong.Name = "textBoxLuong";
+            this.textBoxLuong.Size = new System.Drawing.Size(330, 22);
+            this.textBoxLuong.TabIndex = 15;
+            // 
+            // checkBoxNam
+            // 
+            this.checkBoxNam.AutoSize = true;
+            this.checkBoxNam.Location = new System.Drawing.Point(138, 102);
+            this.checkBoxNam.Name = "checkBoxNam";
+            this.checkBoxNam.Size = new System.Drawing.Size(59, 21);
+            this.checkBoxNam.TabIndex = 16;
+            this.checkBoxNam.Text = "Nam";
+            this.checkBoxNam.UseVisualStyleBackColor = true;
+            this.checkBoxNam.CheckedChanged += new System.EventHandler(this.CheckBoxNam_CheckedChanged);
+            // 
+            // checkBoxNu
+            // 
+            this.checkBoxNu.AutoSize = true;
+            this.checkBoxNu.Location = new System.Drawing.Point(252, 102);
+            this.checkBoxNu.Name = "checkBoxNu";
+            this.checkBoxNu.Size = new System.Drawing.Size(48, 21);
+            this.checkBoxNu.TabIndex = 17;
+            this.checkBoxNu.Text = "Nữ";
+            this.checkBoxNu.UseVisualStyleBackColor = true;
+            this.checkBoxNu.CheckedChanged += new System.EventHandler(this.CheckBoxNu_CheckedChanged);
+            // 
+            // NhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.checkBoxNu);
+            this.Controls.Add(this.checkBoxNam);
+            this.Controls.Add(this.textBoxLuong);
+            this.Controls.Add(this.textBoxChucVu);
+            this.Controls.Add(this.textBoxDiaChi);
+            this.Controls.Add(this.textBoxSDT);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxTen);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "NhanVien";
+            this.Size = new System.Drawing.Size(969, 190);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxTen;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxSDT;
+        private System.Windows.Forms.TextBox textBoxDiaChi;
+        private System.Windows.Forms.TextBox textBoxChucVu;
+        private System.Windows.Forms.TextBox textBoxLuong;
+        private System.Windows.Forms.CheckBox checkBoxNam;
+        private System.Windows.Forms.CheckBox checkBoxNu;
+    }
+}
---
+++ Gym management application/UI/NhanVien.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gym_management_appication.UI
+{
+    public partial class NhanVien : UserControl
+    {
+        public NhanVien()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBoxNam_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.checkBoxNam.Checked == true)
+                this.checkBoxNu.Checked = false;
+        }
+
+        private void CheckBoxNu_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.checkBoxNu.Checked == true)
+                this.checkBoxNam.Checked = false;
+
+        }
+    }
+}
