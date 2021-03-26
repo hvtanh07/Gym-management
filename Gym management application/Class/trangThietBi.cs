e74f4cab2da8ceb7a9e8fca6c01114fc39ca0151
hvtanh07 <32663098+hvtanh07@users.noreply.github.com>
2021-03-26 15:42:20 +0700

    bug fixed

--- Gym management application/Class/trangThietBi.cs
+++ Gym management application/Class/trangThietBi.cs
@@ -10,7 +10,6 @@ namespace Gym_management_appication.Class
     {
         public string ten { get; set; }
         public string ma { get; set; }
-        public int soLuong { get; set; }
         public string tinhTrang { get; set; }
         public DateTime ngayMua { get; set; }
         public int giaThanh { get; set; }
--- TrangThietBi_dummy.sql
+++ TrangThietBi_dummy.sql
@@ -5,7 +5,6 @@ use GymManagement
 create table TrangThietBi (
 	ten nvarchar(50),
 	ma nvarchar(10),
-	soLuong int,
 	tinhTrang nvarchar(50),
 	ngayMua date,
 	giaThanh int,
@@ -18,24 +17,22 @@ create table TrangThietBi (
 
 --dummy Data--
 SET DATEFORMAT dmy;  
-insert into TrangThietBi (ten, ma, soLuong, tinhTrang, ngayMua, giaThanh, baoHanh, ngayHetHanBaoHanh, hangSanXuat) 
-values (N'Tạ tay','Ta-001',12,N'Còn tốt','04/05/2020',500,0,'04/05/2020',N'workoutNow') 
+insert into TrangThietBi (ten, ma, tinhTrang, ngayMua, giaThanh, baoHanh, ngayHetHanBaoHanh, hangSanXuat) 
+values (N'Tạ tay','Ta-001',N'Còn tốt','04/05/2020',500,0,'04/05/2020',N'workoutNow') 
 
-insert into TrangThietBi (ten, ma, soLuong, tinhTrang, ngayMua, giaThanh, baoHanh, ngayHetHanBaoHanh, hangSanXuat)
-values (N'Khung gánh tạ trợ lực','Ma-001',5,N'Còn tốt','15/07/2020',12000,6,'15/01/2021',N'workoutNow')
+insert into TrangThietBi (ten, ma, tinhTrang, ngayMua, giaThanh, baoHanh, ngayHetHanBaoHanh, hangSanXuat)
+values (N'Khung gánh tạ trợ lực','Ma-001',N'Còn tốt','15/07/2020',12000,6,'15/01/2021',N'workoutNow')
 
-insert into TrangThietBi (ten, ma, soLuong, tinhTrang, ngayMua, giaThanh, baoHanh, ngayHetHanBaoHanh, hangSanXuat)
-values (N'Ghế tập tạ','Ta-002',2,N'Còn tốt','19/05/2020',800,6,'19/11/2020',N'workoutNow') 
+insert into TrangThietBi (ten, ma, tinhTrang, ngayMua, giaThanh, baoHanh, ngayHetHanBaoHanh, hangSanXuat)
+values (N'Ghế tập tạ','Ta-002',N'Còn tốt','19/05/2020',800,6,'19/11/2020',N'workoutNow') 
 
-insert into TrangThietBi (ten, ma, soLuong, tinhTrang, ngayMua, giaThanh, baoHanh, ngayHetHanBaoHanh, hangSanXuat)
-values (N'Máy tập ngực','Ma-002',12,N'Hỏng','20/05/2020',1000,12,'20/05/2021',N'workoutLater') 
+insert into TrangThietBi (ten, ma, tinhTrang, ngayMua, giaThanh, baoHanh, ngayHetHanBaoHanh, hangSanXuat)
+values (N'Máy tập ngực','Ma-002',N'Hỏng','20/05/2020',1000,12,'20/05/2021',N'workoutLater') 
 
-insert into TrangThietBi (ten, ma, soLuong, tinhTrang, ngayMua, giaThanh, baoHanh, ngayHetHanBaoHanh, hangSanXuat)
-values (N'Máy tập đùi trong','Ma-003',3,N'Còn tốt','27/10/2020',13000,12,'27/10/2021',N'workoutNo') 
+insert into TrangThietBi (ten, ma, tinhTrang, ngayMua, giaThanh, baoHanh, ngayHetHanBaoHanh, hangSanXuat)
+values (N'Máy tập đùi trong','Ma-003',N'Còn tốt','27/10/2020',13000,12,'27/10/2021',N'workoutNo') 
 
-insert into TrangThietBi (ten, ma, soLuong, tinhTrang, ngayMua, giaThanh, baoHanh, ngayHetHanBaoHanh, hangSanXuat) 
-values (N'Máy tập đùi ngoài','Ma-004',1,N'Trong kho','27/05/2020',10000,12,'27/05/2021',N'workoutNo') 
+insert into TrangThietBi (ten, ma, tinhTrang, ngayMua, giaThanh, baoHanh, ngayHetHanBaoHanh, hangSanXuat) 
+values (N'Máy tập đùi ngoài','Ma-004',N'Trong kho','27/05/2020',10000,12,'27/05/2021',N'workoutNo') 
 
 drop table TrangThietBi
-
-update TrangThietBi set ten = N'Tạ tay', soLuong=12, tinhTrang = N'Đang bảo trì', ngayMua=5/4/2020 12:00:00 AM, giaThanh=500, baoHanh=0, ngayHetHanBaoHanh=5/4/2020 12:00:00 AM, hangSanXuat=workoutNow where ma ='Ta-001'
